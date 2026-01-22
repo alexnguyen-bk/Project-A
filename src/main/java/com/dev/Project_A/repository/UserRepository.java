232493dfa822a2de51c40be36f2a78b6a94176ba
CNPM_Pro <duythinhnguyen2005@gmail.com>
2026-01-23 01:37:59 +0700

    First commit

---
+++ src/main/java/com/dev/Project_A/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.dev.Project_A.repository;
+
+import com.dev.Project_A.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository <User,String> {
+
+}
